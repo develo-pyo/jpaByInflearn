76da9d78868b021a78cb9e3a1a4f8d29040af001
jungpyo <jungpyo9@gmail.com>
2022-07-20 21:47:23 +0900

    commit delete note.txt -> read.me

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +16,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
+    //@JsonIgnore //의도치 않은 Entity 내의 Getter 는 @JsonIgnore 어노테이션을 멤버변수에 붙여 반환되지 않도록 설정이 가능
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
